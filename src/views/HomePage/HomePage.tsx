f89ec3cfb27463caefa7aaeabc810c5d9954c9f2
Michael Reagan <mreagan@assurecare.com>
2024-03-24 17:34:23 -0400

    Update text on home page

--- src/views/HomePage/HomePage.tsx
+++ src/views/HomePage/HomePage.tsx
@@ -4,8 +4,8 @@ import classes from './HomePage.module.scss';
 const HomePage = () => {
   return (
     <div className={classes.home}>
-      This is the home page!
-      <Summary></Summary>
+      This is the home page where you can get an overview - current surplus,
+      recent transactions, monthly progress, safety net, upcoming bills
     </div>
   );
 };
